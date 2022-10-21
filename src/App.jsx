24195f8ed18b7d5613925c461950fdddae7c4e0a
Yuri Ruas <ysdor.dev@gmail.com>
2022-10-21 19:27:13 -0300

    estrutura da aplicação - componente: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import { Post } from './Post'
+
+import styles from './App.module.css'
 
 import './global.css'
 
@@ -8,15 +11,21 @@ export function App() {
     <div>
       <Header />
 
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad nisi maxime rem esse suscipit magnam nostrum placeat debitis dolorum pariatur possimus laudantium natus, quos quidem culpa et obcaecati itaque officia?"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
 
-      <Post
-        author="Gabriel Buzzi"
-        content="Um novo post muito legal"
-      />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad nisi maxime rem esse suscipit magnam nostrum placeat debitis dolorum pariatur possimus laudantium natus, quos quidem culpa et obcaecati itaque officia?"
+          />
+
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
