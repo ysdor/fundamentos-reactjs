911519e361be43d4ef2a76b3f34f1bb657243d51
Yuri Ruas <ysdor.dev@gmail.com>
2022-10-21 18:33:48 -0300

    estrutura da aplicação - css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="Diego Fernandes"
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad nisi maxime rem esse suscipit magnam nostrum placeat debitis dolorum pariatur possimus laudantium natus, quos quidem culpa et obcaecati itaque officia?"
