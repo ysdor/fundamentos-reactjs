36c35a0a846c6aca0e84bcdec0b59a0d520f7e32
Yuri Ruas <ysdor.dev@gmail.com>
2022-10-21 18:17:36 -0300

    iniciando com react - propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad nisi maxime rem esse suscipit magnam nostrum placeat debitis dolorum pariatur possimus laudantium natus, quos quidem culpa et obcaecati itaque officia?"
+      />
+
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }
