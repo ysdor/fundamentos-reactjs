1ef10f554ebcad9e55d597ca6e008faaef8b8556
Yuri Ruas <ysdor.dev@gmail.com>
2022-10-22 18:26:30 -0300

    os motores do react - comunicação entre componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,11 @@ import { Avatar } from './Avatar'
 
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/ysdor.png" />
@@ -17,7 +21,7 @@ export function Comment({ content }) {
               <time title="21 de Outubro às 19:10h" dateTime="2022-10-21 19:10:30">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
